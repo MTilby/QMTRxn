07f56f5082c07dfaab16ef1079aa3912aa28f342
Michael Tilby <wo24546@bristol.ac.uk>
2026-01-05 12:44:48 +0000

    .py scripts for package initial

--- QMTRxn/QMTRxn.py
+++ QMTRxn/QMTRxn.py
@@ -1,12 +1,21 @@
-from argparse import ArgumentParser
+import csv
 import os
+from argparse import ArgumentParser
 
-### Get command line arguments ###
+### set current directory ###
+
+current_dir = os.getcwd()
+
+### Function for command line arguments ###
+
 def GetArgs():
     """Get command line arguments"""
+
     parser = ArgumentParser()
 
-    parser.add_argument("-dir", "--directory", type=str, help="searches in subdirectories for ORCA output files")
+    parser.add_argument("-csv", "--csv", type=str, help="name of .csv file with information on data to extract")
+    parser.add_argument("-p", "--project", type=str, help="name of the directory to iterate")
+    parser.add_argument("-dir", "--directory", action="store_true", help="searches in subdirectories for ORCA output files")
 
     args = parser.parse_args()
 
@@ -15,36 +24,69 @@ def GetArgs():
 if __name__ == "__main__":
     args = GetArgs()
 
-### note I only want this to contain key word arguments to dictate decisions - no other code
+### Function for file extraction from .csv - potentially move to other script for clarity - and allow passing of other data types in a jupyter notebook ###
+
+def csvfile(csvfile):
+    """Read the CSV file and return a dictionary of calculation files and parameters."""
+    with open(csvfile, newline='', encoding='utf-8-sig') as csvfile:
+        reader = csv.DictReader(csvfile)
+        csv_data = {}
+
+        for row in reader:
+            row = {key.lower().replace("_", "" ""): value if isinstance(value, str) else value for key, value in row.items()}
+            filepath = os.path.join(current_dir,row.get("calc", "").strip())
+            
+            if os.path.isfile(f"{filepath}.property.txt"):
+                csv_data[row.get("calc", "").strip()] = f"{filepath}.property.txt"
+
+            elif os.path.isfile(f"{filepath}.out"):
+                csv_data[row.get("calc", "").strip()] = f"{filepath}.out"
+
+    return csv_data
+
+
+### .csv handling ###
+
+if args.csv:
+    csv_filename = f"{args.csv}.csv" if not args.csv.endswith(".csv") else args.csv
+    csv_file = os.path.join(current_dir, csv_filename)
+    calc_files = csvfile(csv_file)
+
+### set directories to use ###
+
+else:
+
+    if args.project:
+        base_dir = os.path.join(current_dir, args.project)
+    else:
+        base_dir = current_dir
+
+    if args.directory:
+        roots = [os.path.join(base_dir, d) for d in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, d))]
+    else:
+        roots = [base_dir]
+
+    ### obtain data from ORCA output files ###
+
+    calc_files = {}
+
+    for root in roots:
+        for file in os.listdir(root):
+            path = os.path.join(root, file)
+
+
+            if file.endswith(".property.txt"):
+                calc = file.removesuffix(".property.txt")
+                calc_files[calc] = path
+
+            elif file.endswith(".out") and file.count(".") == 1:
+                calc = file.removesuffix(".out")
+                calc_files.setdefault(calc, path)
+
+print(calc_files)
 
 
 
 
 
-
-
-### uses command line arguments ###
-base_dir = args.directory if args.directory else '.'
-
-### finds directories ###
-exclude = ('_SPC', 'IRC')
-ts_dirs = [
-    entry.name
-    for entry in os.scandir(base_dir)
-    if entry.is_dir()
-    and entry.name.startswith('TS')
-    and not any(x in entry.name for x in exclude)
-]
-
-print(ts_dirs)
-### extract the data ###
-for name in ts_dirs:
-    sm_SPC = os.path.join(base_dir, name + '_IRC_B_SPC', name + '_IRC_B_SPC.out')
-    p_SPC = os.path.join(base_dir, name + '_IRC_F_SPC', name + '_IRC_F_SPC.out')
-    ts_SPC = os.path.join(base_dir, name + '_SPC', name + '_SPC.out')
-    sm = os.path.join(base_dir, name + '_IRC_B', name + '_IRC_B.out')
-    p = os.path.join(base_dir, name + '_IRC_F', name + '_IRC_F.out')
-    ts = os.path.join(base_dir, name, name + '.out')
-    ts_XYZ = os.path.join(base_dir, name, name + '.xyz')
-    print(ts_XYZ)
 
